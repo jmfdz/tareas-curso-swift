682764f8a2ed89a3064d9e76b844f3f21b081cdc
jmfdz <josema.fernandez@ua.es>
2016-09-06 18:53:42 +0200

    Tarea semana 2 completada

---
+++ tarea-semana-2.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Tarea semana 2 del curso Aprendiendo Swift
+
+import UIKit
+
+let rango = 0...100
+
+for indice in rango {
+    
+    if indice % 5 == 0 {
+        print ("#El número \(indice) Bingo!!!")
+    }
+    
+    if indice % 2 == 0 {
+        print ("#El número \(indice) par!!!")
+    }else {
+        print ("#El número \(indice) impar!!!")
+    }
+    
+    if indice >= 30 && indice <= 40 {
+        print ("#El número \(indice) Viva Swift!!!")
+    }
+}
